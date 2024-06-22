423f2d12d340b26ab9063609dfc0bf5ef58330e8
Giovanni Enrico Loni <65892886+genricoloni@users.noreply.github.com>
2024-06-22 16:17:46 +0200

    chore: Update protocolM1 struct to include EPHkeyLength and serialize/deserialize methods

--- src/crypto/secureProtocol.hpp
+++ src/crypto/secureProtocol.hpp
@@ -11,7 +11,7 @@
 #include "../const.hpp"
 
 struct protocolM1 {
-    uint32_t keyLength;
+    uint32_t EPHkeyLength;
     uint8_t EPHKey[EPH_KEY_SIZE];
     // RSA public key
     uint32_t publicKeyLength;
@@ -43,7 +43,7 @@ struct protocolM1 {
             throw std::invalid_argument("Public key is NULL");
         }
 
-        this->keyLength = keyLength;
+        this->EPHkeyLength = keyLength;
         this->publicKeyLength = publicKeyLength;
 
         memcpy(this->EPHKey, ephKey.data(), keyLength);
@@ -51,4 +51,52 @@ struct protocolM1 {
 
     }
 
+    void serialize(std::vector<uint8_t>& buffer) {
+        int position = 0;
+        buffer.resize(GetSize());
+
+        uint32_t EPHkeySizeNetwork = htonl(EPHkeyLength);
+        std::memcpy(buffer.data() + position, &EPHkeySizeNetwork, sizeof(EPHkeySizeNetwork));
+        position += sizeof(EPHkeySizeNetwork);
+
+        std::memcpy(buffer.data() + position, EPHKey, EPHkeyLength);
+        position += EPHkeyLength;
+
+        uint32_t publicKeySizeNetwork = htonl(publicKeyLength);
+        std::memcpy(buffer.data() + position, &publicKeySizeNetwork, sizeof(publicKeySizeNetwork));
+
+        position += sizeof(publicKeySizeNetwork);
+        std::memcpy(buffer.data() + position, publicKey, publicKeyLength);
+
+    }
+
+    static inline int GetSize() {
+        return sizeof(EPHkeyLength) + 
+                sizeof(EPHKey) + 
+                sizeof(publicKeyLength) + 
+                sizeof(publicKey);
+    }
+
+    protocolM1 deserialize(std::vector<uint8_t>& buffer) {
+        protocolM1 m1;
+
+        int position = 0;
+        uint32_t EPHkeySizeNetwork = 0;
+
+        std::memcpy(&EPHkeySizeNetwork, buffer.data() + position, sizeof(EPHkeySizeNetwork));
+        m1.EPHkeyLength = ntohl(EPHkeySizeNetwork);
+        position += sizeof(EPHkeySizeNetwork);
+
+        std::memcpy(m1.EPHKey, 0, sizeof(m1.EPHKey));
+        std::memcpy(m1.EPHKey, buffer.data() + position, sizeof(m1.EPHKey));
+        position += sizeof(m1.EPHKey) * EPH_KEY_SIZE;
+
+        m1.publicKeyLength = ntohl(*(uint32_t*)(buffer.data() + position));
+        position += sizeof(m1.publicKeyLength);
+
+        std::memcpy(m1.publicKey, 0, sizeof(m1.publicKey));
+        std::memcpy(m1.publicKey, buffer.data() + position, m1.publicKeyLength);
+
+        return m1;
+    }
 };
